e56cab758e535882fb42d03ef3013f82d480a1d0
DenDevil72 <74084635+DenDevil72@users.noreply.github.com>
2023-02-02 06:51:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ load_dotenv()
 Bot = Client(
     "GoFile-Bot",
     bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get("API_ID")),
+    api_id=os.environ.get("API_ID"),
     api_hash=os.environ.get("API_HASH")
 )
 
